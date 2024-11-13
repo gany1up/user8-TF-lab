ab0e1cbb0b207b0556541bf14ecbe1f555ade55a
gany <test@test.com>
2024-11-13 16:52:07 +0530

    newlocalfile

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_vpc" "u8vpc1" {
-    cidr_block = "10.111.0.0/16"
-    tags = {
-        Name  = "u8vpc"
-    }
+# resource "aws_vpc" "u8vpc1" {
+#     cidr_block = "10.111.0.0/16"
+#     tags = {
+#         Name  = "u8vpc"
+#     }
 }
 resource "local_file" "test" {
-    filename = "u8file"
-    content = "have this content"
+    filename = "./NEWu8file"
+    content = "NEWhave this content"
   
 }
